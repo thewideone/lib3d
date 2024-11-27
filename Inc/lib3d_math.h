b67d76465ad020c14186601539623bf18d36a4c9
thewideone <szymon.kajda@onet.pl>
2024-11-27 20:07:51 +0100

    Started adding basic typedefs

--- Inc/lib3d_math.h
+++ Inc/lib3d_math.h
@@ -24,7 +24,30 @@ typedef struct {
 	rtnl_t x;
 	rtnl_t y;
 	rtnl_t z;
-	rtnl_t h;
+	rtnl_t h;	// homogeneous coordinates are used
 } vec4_t;
+
+typedef struct {
+	uint32_t vert_ids[2];
+	bool is_visible;	// or partially?
+	bool is_boundary;
+	bool is_face_outer;
+} edge_t;
+
+typedef struct {
+	uint32_t vert_ids[3];
+	uint32_t edge_ids[3];
+	bool is_visible;
+} tri_t;
+
+typedef struct {
+	tri_t* tris;	// of the original model
+	edge_t* edges;				// seems to be dynamic
+	vec4_t* vertices_world;		// please make them static ;-;
+	vec4_t* vertices_projected;
+	uint32_t tri_count;
+	uint32_t vert_count;
+	uint32_t edge_count;
+} mesh_t;
 
 #endif // _LIB3D_MATH3D_H_
