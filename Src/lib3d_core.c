c476e308c607e1bb148ff21bf710b8edbbc3e72b
thewideone <szymon.kajda@onet.pl>
2026-07-12 19:07:20 +0200

    Tried implementing hidden line elimination as in previous attempts to this project

--- Src/lib3d_core.c
+++ Src/lib3d_core.c
@@ -412,6 +412,365 @@ l3d_err_t l3d_drawObjects(const l3d_scene_t *scene) {
 }
 
 // 
+// Perform backface culling in given scene.
+// 
+// Return:
+// 	L3D_WRONG_PARAM if wrong parameter,
+// 	L3D_DATA_EMPTY if no active camera is set,
+// 	L3D_OK otherwise.
+// 
+l3d_err_t l3d_cull_backfaces(const l3d_scene_t *scene) {
+	if (scene == NULL)
+		return L3D_WRONG_PARAM;
+	
+	// Get active camera
+	l3d_camera_t *active_cam = l3d_scene_getActiveCamera(scene);
+
+	if (active_cam == NULL)
+		return L3D_DATA_EMPTY;
+
+	// For each face
+	for (uint16_t tri_id = 0; tri_id < scene->model_tri_count; tri_id++) {
+		// Calculate face normal
+		uint16_t v0_id = scene->model_tri_data[tri_id * 3 + 0];	// each tri is a 3x uin16_t entry
+		uint16_t v1_id = scene->model_tri_data[tri_id * 3 + 1];
+		uint16_t v2_id = scene->model_tri_data[tri_id * 3 + 2];
+
+		// L3D_DEBUG_PRINT("v0_id = %d, v1_id = %d, v2_id = %d\n", v0_id, v1_id, v2_id);
+
+		l3d_vec4_t v0 = scene->vertices_world[v0_id];
+		l3d_vec4_t v1 = scene->vertices_world[v1_id];
+		l3d_vec4_t v2 = scene->vertices_world[v2_id];
+
+		// L3D_DEBUG_PRINT_VEC4(v0);
+		// L3D_DEBUG_PRINT_VEC4(v1);
+		// L3D_DEBUG_PRINT_VEC4(v2);
+
+		l3d_vec4_t e1 = l3d_vec4_sub(&v2, &v1);
+		l3d_vec4_t e2 = l3d_vec4_sub(&v0, &v1);
+
+		l3d_vec4_t normal = l3d_vec4_crossProduct(&e1, &e2);
+		normal = l3d_vec4_normalise(&normal);
+
+		// Get the camera ray
+		l3d_vec4_t cam_ray = l3d_vec4_sub(&v0, &(active_cam->local_pos)); // really pos? not look dir?
+
+		// If dot product of the camera ray and face normal is below the threshold
+		// the face is visible
+		if (l3d_vec4_dotProduct(&normal, &cam_ray) < l3d_floatToRational(0.0f)) {
+			// How to store face visibility?
+			// Is it needed? Visibility could be stored per edge
+		}
+	}
+	
+	return L3D_OK;
+}
+
+// 
+// Return the bounding box in view space (projected 2D)
+// of a triangle of given ID in given scene.
+// 
+// scene	- input scene
+// tri_id	- absolute ID of triangle of interest
+// v_min	- first corner of the bounding box
+// v_max	- second corner of the bounding box
+// 
+// Return L3D_WRONG_PARAM if wrong parameters,
+// L3D_OK otherwise.
+// 
+l3d_err_t l3d_tri_get_bbox_2d(const l3d_scene_t *scene, uint16_t tri_id, l3d_vec4_t *v_min, l3d_vec4_t *v_max) {
+	if (scene == NULL || tri_id >= scene->model_tri_count || scene->model_tri_data == NULL)
+		return L3D_WRONG_PARAM;
+	
+	uint16_t v0_id = scene->model_tri_data[tri_id * 3 + 0];	// each tri is a 3x uin16_t entry
+	uint16_t v1_id = scene->model_tri_data[tri_id * 3 + 1];
+	uint16_t v2_id = scene->model_tri_data[tri_id * 3 + 2];
+
+	// L3D_DEBUG_PRINT("v0_id = %d, v1_id = %d, v2_id = %d\n", v0_id, v1_id, v2_id);
+
+	l3d_vec4_t v0 = scene->vertices_projected[v0_id];
+	l3d_vec4_t v1 = scene->vertices_projected[v1_id];
+	l3d_vec4_t v2 = scene->vertices_projected[v2_id];
+
+	// L3D_DEBUG_PRINT_VEC4(v0);
+	// L3D_DEBUG_PRINT_VEC4(v1);
+	// L3D_DEBUG_PRINT_VEC4(v2);
+	
+	*v_min = v0;
+	*v_max = v0;
+
+	if (v1.x < v_min->x)
+		v_min->x = v1.x;
+	if (v1.y < v_min->y)
+		v_min->y = v1.y;
+	// if (v1.z < v_min->z)
+	// 	v_min->z = v1.z;
+
+	if (v2.x < v_min->x)
+		v_min->x = v2.x;
+	if (v2.y < v_min->y)
+		v_min->y = v2.y;
+	// if (v2.z < v_min->z)
+	// 	v_min->z = v2.z;
+
+	if (v1.x > v_max->x)
+		v_max->x = v1.x;
+	if (v1.y > v_max->y)
+		v_max->y = v1.y;
+	// if (v1.z > v_max->z)
+	// 	v_max->z = v1.z;
+	
+	if (v2.x > v_max->x)
+		v_max->x = v2.x;
+	if (v2.y > v_max->y)
+		v_max->y = v2.y;
+	// if (v2.z > v_max->z)
+	// 	v_max->z = v2.z;
+	
+	v_min->h = l3d_floatToRational(1.0f);
+	v_max->h = l3d_floatToRational(1.0f);
+
+	return L3D_OK;
+}
+
+
+// 
+// Taken from:
+// "Fast Minimum Storage Ray/Triangle Intersection"
+// by Tomas Moller, Ben Trumbore
+// https://web.archive.org/web/20170517125238/http://www.cs.virginia.edu/~gfx/courses/2003/ImageSynthesis/papers/Acceleration/Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf
+// 
+// Found at:
+// https://math.stackexchange.com/a/2334999
+// 
+// TODO:
+// 	- tweak L3D_EPSILON to improve stability of the algorithm
+// 
+// This algorithm operates in world space. (?) I think so at least
+// It's intended to use in raytracing.
+// 
+// orig			- origin of the ray R(t)
+// dir			- must be normalised; direction of the ray R(t)
+// v0, v1, v2	- vertices of the triangle to test
+// t			- distance from the ray origin to the plane on which the triangle lies (to the intersection point)
+// (u, v)		- coordinates of the intersection inside of the triangle
+// 
+#define L3D_TEST_CULL
+int l3d_intersect_tri(const l3d_vec4_t *orig, const l3d_vec4_t *dir,
+						const l3d_vec4_t *v0, const l3d_vec4_t *v1, const l3d_vec4_t *v2,
+						l3d_rtnl_t *t, l3d_rtnl_t *u, l3d_rtnl_t *v) {
+	l3d_vec4_t edge1, edge2, tvec, pvec, qvec;
+	l3d_rtnl_t det, inv_det;
+
+	// Find vectors for two edges sharing v0
+	edge1 = l3d_vec4_sub(&v1, &v0);
+	edge2 = l3d_vec4_sub(&v2, &v0);
+
+	// Begin calculating determinant - also used to calculate U parameter
+	pvec = l3d_vec4_crossProduct(dir, &edge2);
+
+	// If determinant is near zero, ray lies in plane of triangle
+	det = l3d_vec4_dotProduct(&edge1, &pvec);
+
+#ifdef L3D_TEST_CULL
+	if (det < L3D_EPSILON_RTNL)
+		return 0;
+	
+	// Calculate distance from v0 to ray origin
+	tvec = l3d_vec4_sub(orig, &pvec);
+
+	// Calculate U parameter and test bounds
+	*u = l3d_vec4_dotProduct(&tvec, &pvec);
+	if (*u < l3d_floatToRational(0.0f) || *u > det)
+		return 0;
+	
+	// Prepare to test V parameter
+	qvec = l3d_vec4_crossProduct(&tvec, &edge1);
+
+	// Calculate V parameter and test bounds
+	*v = l3d_vec4_dotProduct(dir, &qvec);
+	if (*v < l3d_floatToRational(0.0f) || *u + *v > det)
+		return 0;
+	
+	// Calculate t, scale parameters, ray intersects triangle
+	*t = l3d_vec4_dotProduct(&edge2, &qvec);
+#ifdef L3D_USE_FIXED_POINT_ARITHMETIC
+	inv_det = l3d_fixedDiv( l3d_floatToFixed(1.0f), det );
+	*t = l3d_fixedMul(*t, inv_det);
+	*u = l3d_fixedMul(*u, inv_det);
+	*v = l3d_fixedMul(*v, inv_det);
+#else
+	inv_det = 1.0f / det;
+	*t *= inv_det;
+	*u *= inv_det;
+	*v *= inv_det;
+#endif
+#else // non-culling branch
+	if (det > -L3D_EPSILON_RTNL && det < L3D_EPSILON_RTNL)
+		return 0;
+	
+	#ifdef L3D_USE_FIXED_POINT_ARITHMETIC
+	inv_det = l3d_fixedDiv( l3d_floatToFixed(1.0f), det );
+#else
+	inv_det = 1.0f / det;
+#endif
+
+	// Calculate distance from v0 to ray origin
+	tvec = l3d_vec4_sub(orig, v0);
+
+	// Calculate U parameter and test bounds
+	// *u = l3d_vec4_dotProduct(&tvec, &pvec) * inv_det;
+#ifdef L3D_USE_FIXED_POINT_ARITHMETIC
+	*u = l3d_fixedMul( l3d_vec4_dotProduct(&tvec, &pvec), inv_det );
+#else
+	*u = l3d_vec4_dotProduct(&tvec, &pvec) * inv_det;
+#endif
+	if (*u < l3d_floatToRational(0.0f) || *u > l3d_floatToRational(1.0f))
+		return 0;
+	
+	// Prepare to test V parameter
+	qvec = l3d_vec4_crossProduct(&tvec, &edge1);
+
+	// Calculate V parameter and test bounds
+#ifdef L3D_USE_FIXED_POINT_ARITHMETIC
+	*v = l3d_fixedMul( l3d_vec4_dotProduct(dir, &qvec), inv_det );
+#else
+	*v = l3d_vec4_dotProduct(dir, &qvec) * inv_det;
+#endif
+	if (*v < l3d_floatToRational(0.0f) || *u + *v > l3d_floatToRational(1.0f))
+		return 0;
+	
+	// Calculate t, ray intersects triangle
+#ifdef L3D_USE_FIXED_POINT_ARITHMETIC
+	*t = l3d_fixedMul( l3d_vec4_dotProduct(&edge2, &qvec), inv_det );
+#else
+	*t = l3d_vec4_dotProduct(&edge2, &qvec) * inv_det;
+#endif
+#endif
+	return 1;
+}
+
+// 
+// Draw every object in given scene,
+// with Hidden Line Elimination algorithm.
+// 
+#define MAX_FACE_INTERSECTIONS 64
+l3d_err_t l3d_drawObjectsHLE(const l3d_scene_t *scene) {
+	if (scene == NULL ||
+		scene->objects == NULL ||
+		scene->edge_flags == NULL ||
+		scene->model_edge_data == NULL ||
+		scene->vertices_projected == NULL )
+		return L3D_WRONG_PARAM;
+
+	// l3d_err_t ret = L3D_OK;
+
+	// Perform backface culling (optional optimisation)
+
+	for (uint16_t edge_id = 0; edge_id < scene->model_edge_count; edge_id++) {
+		uint16_t e_v0_id = scene->model_edge_data[edge_id * 3 + 0];	// each edge is a 3x uin16_t entry
+		uint16_t e_v1_id = scene->model_edge_data[edge_id * 3 + 1];
+
+		// L3D_DEBUG_PRINT("e_v0_id = %d, e_v1_id = %d\n", e_v0_id, e_v1_id);
+
+		l3d_vec4_t e_v0 = scene->vertices_projected[e_v0_id];
+		l3d_vec4_t e_v1 = scene->vertices_projected[e_v1_id];
+
+		L3D_DEBUG_PRINT_VEC4(e_v0);
+		L3D_DEBUG_PRINT_VEC4(e_v1);
+
+		l3d_vec4_t e_bbox_min = e_v0, e_bbox_max = e_v0;
+
+		if (e_v1.x < e_bbox_min.x)
+			e_bbox_min.x = e_v1.x;
+		if (e_v1.y < e_bbox_min.y)
+			e_bbox_min.y = e_v1.y;
+		
+		if (e_v1.x > e_bbox_max.x)
+			e_bbox_max.x = e_v1.x;
+		if (e_v1.y > e_bbox_max.y)
+			e_bbox_max.y = e_v1.y;
+
+		L3D_DEBUG_PRINT_VEC4(e_bbox_min);
+		L3D_DEBUG_PRINT_VEC4(e_bbox_max);
+
+		// Calculate edge origin and direction
+		l3d_vec4_t e_orig = e_v0;
+		l3d_vec4_t e_dir = l3d_vec4_sub(&e_v1, &e_v0);
+		e_dir = l3d_vec4_normalise(&e_dir);
+
+		l3d_rtnl_t t_list[MAX_FACE_INTERSECTIONS] = {l3d_floatToRational(0.0f)};
+		int8_t delta[MAX_FACE_INTERSECTIONS] = {0};
+		uint16_t n=0;
+
+		for (uint16_t tri_id = 0; tri_id < scene->model_tri_count; tri_id++) {
+			// if bbox2D(e) does not intersect bbox2D(f):
+			// 	continue
+
+			l3d_vec4_t tri_bbox_min, tri_bbox_max;
+			l3d_err_t ret = l3d_tri_get_bbox_2d(scene, tri_id, &tri_bbox_min, &tri_bbox_max);
+
+			if (ret != L3D_OK)
+				return ret;
+			
+			if (e_bbox_min.x > tri_bbox_max.x && e_bbox_min.y > tri_bbox_max.y ||
+				e_bbox_max.x < tri_bbox_min.x && e_bbox_max.y < tri_bbox_min.y)
+				continue;
+
+			// if segmentIntersectsPolygon2D(e_proj, f_proj):
+			// 	compute t_in, t_out along edge param [0,1]
+			// 	t_list[n] = t_in; delta[n] = +1; n++
+			// 	t_list[n] = t_out; delta[n] = -1; n++
+
+			uint16_t tri_v0_id = scene->model_tri_data[tri_id * 3 + 0];	// each tri is a 3x uin16_t entry
+			uint16_t tri_v1_id = scene->model_tri_data[tri_id * 3 + 1];
+			uint16_t tri_v2_id = scene->model_tri_data[tri_id * 3 + 2];
+
+			// L3D_DEBUG_PRINT("v0_id = %d, v1_id = %d, v2_id = %d\n", v0_id, v1_id, v2_id);
+
+			l3d_vec4_t tri_v0 = scene->vertices_world[tri_v0_id];
+			l3d_vec4_t tri_v1 = scene->vertices_world[tri_v1_id];
+			l3d_vec4_t tri_v2 = scene->vertices_world[tri_v2_id];
+
+			// L3D_DEBUG_PRINT_VEC4(v0);
+			// L3D_DEBUG_PRINT_VEC4(v1);
+			// L3D_DEBUG_PRINT_VEC4(v2);
+
+			l3d_rtnl_t t, u, v;
+			// DIFFERENCE TO THE ALGORITHM PROVIDED BY CHATGPT:
+			// 	- l3d_intersect_tri() operates in world space, ChatGPT said to test intersection in 2D
+			// But maybe this algorithm can be used in 2D too?
+			// 	- need to test
+			if (l3d_intersect_tri(&e_orig, &e_dir, &tri_v0, &tri_v1, &tri_v2, &t, &u, &v)) {
+				// t_in computed as t
+				// need to compute t_out... how?
+			}
+			// No intersection, skip
+			else {
+				continue;
+			}
+		}
+
+		// if n == 0:
+		// 	drawSegment(A_proj, B_proj)
+		// 	continue
+		// sortPairsByT(t_list, delta, n)
+		// count = 0
+		// t_prev = 0.0
+		// for i = 0..n-1:
+		// 	t_curr = t_list[i]
+		// 	if count == 0 and t_curr > t_prev:
+		// 	drawSegment(P(t_prev).proj, P(t_curr).proj)
+		// 	count += delta[i]
+		// 	t_prev = t_curr
+		// if count == 0 and t_prev < 1.0:
+		// 	drawSegment(P(t_prev).proj, P(1.0).proj)
+	}
+
+	return L3D_OK;
+}
+
+// 
 // Compute projection and view matrices if needed,
 // transform objects into view space,
 // and draw them
@@ -447,7 +806,11 @@ l3d_err_t l3d_processScene(l3d_scene_t *scene) {
 		l3d_transformObjectIntoViewSpace(scene, L3D_OBJ_TYPE_OBJ3D, obj_idx);
 	}
 
-	ret = l3d_drawObjects(scene);
+	// ret = l3d_drawObjects(scene);
+	ret = l3d_drawObjectsHLE(scene);
+
+	// l3d_vec4_t vb1, vb2;
+	// l3d_tri_get_bbox_2d(scene, 0, &vb1, &vb2);
 
 	return ret;
 }
